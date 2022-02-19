7aca960d89b5a1ad206cb40bdc5484b4982cda9c
valeriadelatorrerod <valeriadelatorrerod@outlook.com>
2022-02-19 12:49:32 -0600

    Implementación de función para imprimir variable

---
+++ pythondata1.py
@@ -0,0 +1,12 @@
+var1 = "Hola aprendices"
+var2 = "20"
+
+var3 = 20
+
+# print(var1)
+# print(int(var2) + var3)
+
+def propiedad_variable_integer(valor_numerico):
+  return print("El valor numerico es :>> ", valor_numerico)
+
+propiedad_variable_integer(var3)
